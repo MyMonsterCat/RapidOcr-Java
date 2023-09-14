e966199da7c8b396aa0d621c3d586e8b98c44866
Monster <1264833435@qq.com>
2023-09-14 17:13:21 +0800

    first

---
+++ src/main/java/com/benjaminwan/ocrlibrary/OcrEngine.java
@@ -0,0 +1,68 @@
+package com.benjaminwan.ocrlibrary;
+
+import com.github.monster.OcrUtil;
+
+/**
+ * OCR引擎对象，负责接收参数并执行OCR
+ * 请确保包位置无误，必须在com.benjaminwan.ocrlibrary下
+ * 参数说明请看{@link OcrUtil}
+ */
+public class OcrEngine {
+
+    public OcrEngine(String library) {
+        System.loadLibrary(library);
+    }
+
+    private int padding = 50;
+    private float boxScoreThresh = 0.5f;
+    private float boxThresh = 0.3f;
+    private float unClipRatio = 1.6f;
+    private boolean doAngle = true;
+    private boolean mostAngle = true;
+
+    public void setPadding(int padding) {
+        this.padding = padding;
+    }
+
+    public void setBoxScoreThresh(float boxScoreThresh) {
+        this.boxScoreThresh = boxScoreThresh;
+    }
+
+    public void setBoxThresh(float boxThresh) {
+        this.boxThresh = boxThresh;
+    }
+
+    public void setUnClipRatio(float unClipRatio) {
+        this.unClipRatio = unClipRatio;
+    }
+
+    public void setDoAngle(boolean doAngle) {
+        this.doAngle = doAngle;
+    }
+
+    public void setMostAngle(boolean mostAngle) {
+        this.mostAngle = mostAngle;
+    }
+
+    public OcrResult detect(String input, int maxSideLen) {
+        return detect(input, padding, maxSideLen, boxScoreThresh, boxThresh, unClipRatio, doAngle, mostAngle);
+    }
+
+    public native boolean setNumThread(int numThread);
+
+    public native void initLogger(boolean isConsole, boolean isPartImg, boolean isResultImg);
+
+    public native void enableResultText(String imagePath);
+
+    public native boolean initModels(String modelsDir, String detName, String clsName, String recName, String keysName);
+
+    public native void setGpuIndex(int gpuIndex);
+
+    public native String getVersion();
+
+    public native OcrResult detect(
+        String input, int padding, int maxSideLen,
+        float boxScoreThresh, float boxThresh,
+        float unClipRatio, boolean doAngle, boolean mostAngle
+    );
+}
---
+++ src/main/java/com/benjaminwan/ocrlibrary/OcrResult.java
@@ -0,0 +1,51 @@
+package com.benjaminwan.ocrlibrary;
+
+import java.util.ArrayList;
+
+public class OcrResult extends OcrOutput {
+    private final double dbNetTime;
+    private final ArrayList<TextBlock> textBlocks;
+    private double detectTime;
+    private String strRes;
+
+    public OcrResult(double dbNetTime, ArrayList<TextBlock> textBlocks, double detectTime, String strRes) {
+        this.dbNetTime = dbNetTime;
+        this.textBlocks = textBlocks;
+        this.detectTime = detectTime;
+        this.strRes = strRes;
+    }
+
+    public double getDbNetTime() {
+        return dbNetTime;
+    }
+
+    public ArrayList<TextBlock> getTextBlocks() {
+        return textBlocks;
+    }
+
+    public double getDetectTime() {
+        return detectTime;
+    }
+
+    public void setDetectTime(double detectTime) {
+        this.detectTime = detectTime;
+    }
+
+    public String getStrRes() {
+        return strRes;
+    }
+
+    public void setStrRes(String strRes) {
+        this.strRes = strRes;
+    }
+
+    @Override
+    public String toString() {
+        return "OcrResult{" +
+                "dbNetTime=" + dbNetTime +
+                ", textBlocks=" + textBlocks +
+                ", detectTime=" + detectTime +
+                ", strRes='" + strRes + '\'' +
+                '}';
+    }
+}
