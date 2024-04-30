8915b82fd32a5556482bfef7df1c3758320c1750
Tong Li <31761981+litongjava@users.noreply.github.com>
2024-04-29 22:59:23 -1000

    change log to english (#43)
    * change log to english
    
    * Create readme.md
    
    * change loadFileIfNeeded and initEngine to public
    
    * change getInstance
    
    ---------
    
    Co-authored-by: litongjava <litongjava@qq.com>

---
+++ rapidocr-onnx-models/src/main/resources/models/readme.md
@@ -0,0 +1,5 @@
+# models
+
+Due to file size limitations, the server model is not added by default. If you need the server model, please download it from here and add it to the current directory, then repackage.
+
+- https://huggingface.co/SWHL/RapidOCR/tree/main/PP-OCRv4
--- rapidocr/src/main/java/com/benjaminwan/ocrlibrary/OcrEngine.java
+++ rapidocr/src/main/java/com/benjaminwan/ocrlibrary/OcrEngine.java
@@ -7,12 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Objects;
 
 /**
- * OCR引擎对象，通过JNI与库文件交互
+ * OCR engine object that interacts with library files through JNI.
  */
 @Slf4j
 public class OcrEngine {
 
-    protected void initEngine(Model model, HardwareConfig hardwareConfig) {
+    public void initEngine(Model model, HardwareConfig hardwareConfig) {
         if (!isInit) {
             synchronized (this) {
                 if (!isInit) {
@@ -22,20 +22,20 @@ public class OcrEngine {
                         setGpuIndex(hardwareConfig.getGpuIndex());
                     }
                     if (!initModels(model.getTempDirPath(), model.getDetName(), model.getClsName(), model.getRecName(), model.getKeysName())) {
-                        log.error("模型初始化错误，请检查models路径！model: {}", model);
-                        throw new IllegalArgumentException("模型初始化错误，请检查models/keys路径！");
+                        log.error("Model initialization error, please check the models path! Model: {}", model);
+                        throw new IllegalArgumentException("Model initialization error, please check the models/keys path!");
                     }
                     inferType = model.getModelType();
-                    log.info("推理引擎初始化完成，当前使用的推理引擎为：{}-v{}", inferType, getVersion());
-                    log.info("初始化时模型配置为：{}， 硬件配置为：{}", model, hardwareConfig);
+                    log.info("Inference engine initialized successfully, currently using inference engine: {}-v{}", inferType, getVersion());
+                    log.info("Model configuration during initialization: {}, Hardware configuration: {}", model, hardwareConfig);
                     isInit = true;
                 }
             }
         } else {
             if (!Objects.equals(model.getModelType(), inferType)) {
-                log.warn("引擎已初始化，初始化后不可更换为其他引擎，将继续使用{}推理引擎工作", inferType);
+                log.warn("Engine has been initialized already; switching engines post-initialization is not supported, continuing to use {} inference engine", inferType);
             } else {
-                log.info("当前使用的推理引擎为：{}-v{}", inferType, getVersion());
+                log.info("Currently using inference engine: {}-v{}", inferType, getVersion());
             }
         }
     }
